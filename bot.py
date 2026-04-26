5ed87b963b8fbba8d9719cac7d986e16a7d2f2fa
syedumair787 <umair.gps@gmail.com>
2026-04-26 02:57:06 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,8 @@ STOCKS = [
 ]
 
 def get_data(symbol):
-       try:
+     def get_data(symbol):
+    try:
         url = f"https://stooq.com/q/d/l/?s={symbol.lower()}.pk&i=d"
         df = pd.read_csv(url)
 
